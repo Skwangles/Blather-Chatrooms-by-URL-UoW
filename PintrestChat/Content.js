9cf77999eabd2a28e130b8bd619ff880b376bf83
Skwangles <skwanglesavenue@gmail.com>
2021-05-18 15:38:35 +1200

    floating

--- PintrestChat/Content.js
+++ PintrestChat/Content.js
@@ -1,18 +1,29 @@
- fetch(chrome.runtime.getURL('button.html')).then(r => r.text()).then(html => {
-        document.body.insertAdjacentHTML('beforeend', html);
-     // not using innerHTML as it would break js event listeners of the page
+fetch(chrome.runtime.getURL('button.html')).then(r => r.text()).then(html => {
+  document.body.insertAdjacentHTML('beforeend', html);
+  // not using innerHTML as it would break js event listeners of the page
+});
+
+document.addEventListener('DOMContentLoaded', function () 
+{
+  
+  document.getElementById("item-button").addEventListener('click', function () {
+    console.log("recieved click");
+
+
+
   });
+});
 
 
-chrome.runtime.onMessage.addListener(messageRecieved);
+  chrome.runtime.onMessage.addListener(messageRecieved);
 
 function messageRecieved(request, sender, sendResponse) {
   if (request.message == "urll") {
     sendResponse({ urll: window.location.href });
   }
-  else if (request.message != ""){
+  else if (request.message != "") {
     alert(request.message);
-    sendResponse({urll:null});
+    sendResponse({ urll: null });
   }
   return true;
 }
