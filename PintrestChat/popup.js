47faa336ac0576eb680b23f7c46efd61e304d244
Skwangles <skwanglesavenue@gmail.com>
2021-05-22 23:18:59 +1200

    saved checkbox state

--- PintrestChat/popup.js
+++ PintrestChat/popup.js
@@ -6,12 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
     //
 
     document.getElementById('chat-open').addEventListener('click', setup);//button event listener
-    document.getElementById("showOpenChat").addEventListener('change', function(){
-        if(document.getElementById("showOpenChat").checked == true){
+    document.getElementById("showOpenChat").addEventListener('change', function () {
+        if (document.getElementById("showOpenChat").checked == true) {
             sendMsg("show");
+            chrome.storage.sync.set({ "hidden": "false" });
         }
-        else{
+        else {
             sendMsg("hide");
+            chrome.storage.sync.set({ "hidden": "true" });
         }
     });
     document.getElementById('user-name').addEventListener('change', function () {//updates textboxes when text changes
@@ -20,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('user-id').addEventListener('change', function () {
         chrome.storage.sync.set({ "user": document.getElementById("user-id").value });
     });
-    
+
 
 
     if (document.getElementById("user-name").value == "") {//sets display name to currently saved value
@@ -33,6 +35,15 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
+    chrome.storage.sync.get(["hidden"], function (result) {//sets check box to currently saved status
+        if (result.hidden == "true") {
+            document.getElementById("showOpenChat").checked = false;
+        }
+        else {
+            document.getElementById("showOpenChat").checked = true;
+        }
+    });
+
 
     if (document.getElementById("user-id").value == "") {//sets username value to currently save value
         chrome.storage.sync.get(["user"], function (result) {
@@ -49,18 +60,18 @@ document.addEventListener('DOMContentLoaded', function () {
 //
 //End of DOM load
 //
-function sendMsg(mess){
+function sendMsg(mess) {
     let params =
     {
         active: true,
         currentWindow: true
     }
-chrome.tabs.query(params, function (tabs) {
-    let msg = {
-        message: mess
-    }
-    chrome.tabs.sendMessage(tabs[0].id, msg);
-});
+    chrome.tabs.query(params, function (tabs) {
+        let msg = {
+            message: mess
+        }
+        chrome.tabs.sendMessage(tabs[0].id, msg);
+    });
 }
 
 function setup() {
